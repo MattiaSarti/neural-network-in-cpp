33c2b8f075a84bd50cd59f1ce2d9523274b3c40a
MattiaSarti <nmattia1@gmail.com>
2021-07-16 17:59:05 +0200

    🌴 ADD - dataset plots 🌵

--- create_dataset.py
+++ create_dataset.py
@@ -6,106 +6,174 @@ input feature space.
 
 from os import getcwd
 from os.path import join as os_join
+from typing import Tuple
 
 from matplotlib.pyplot import (
-    colorbar, figure, savefig, scatter, show, title, xlabel, ylabel
+    colorbar, figure, savefig, scatter, show, subplot, title, xlabel, ylabel
 )
-from numpy import insert, savetxt, stack
-from numpy.random import shuffle
+from numpy import insert, ndarray, savetxt, stack
+from numpy.random import seed, shuffle
 from sklearn.datasets import make_swiss_roll
 
 
 GAUSSIAN_NOISE_STD = 0.2
-N_SAMPLES = 10
+N_SAMPLES = 80
 SEED = 0
 VALIDATION_AMOUNT = 0.3
 
 PLOT = True
-SAVE_PLOT = True
+SAVE_PLOTS = True
 SAVE_DATA = True
 
 PROJECT_DIR = getcwd()
 
 
-# creating the dataset:
-dataset_samples, dataset_labels = make_swiss_roll(
-    n_samples=N_SAMPLES,
-    noise=GAUSSIAN_NOISE_STD,
-    random_state=SEED
-)
-
-if PLOT or SAVE_PLOT:
-
-    # plotting the dataset samples in the 2D feature space, color-coded by
-    # label:
-    figure()
-    scatter(
-        x=dataset_samples[:, 0],
-        y=dataset_samples[:, 2],
-        c=dataset_labels
+def create_dataset() -> ndarray:
+    """
+    Create the dataset samples and labels in a reproducible way.
+    """
+    # employing a Swiss Roll dataset by Scikit-Learn, with 3 input features
+    # and a single numerical output:
+    samples, labels = make_swiss_roll(
+        n_samples=N_SAMPLES,
+        noise=GAUSSIAN_NOISE_STD,
+        random_state=SEED
     )
-    xlabel("Feature 1")
-    ylabel("Feature 2")
-    title("Output Value as Color")
-    colorbar()
-
-    if SAVE_PLOT:
-
-        # saving the plot picture:
-        picture_path = os_join(
-            PROJECT_DIR,
-            "readme_pictures",
-            "dataset_plot.png"
-        )
-        savefig(picture_path)
-
-    if PLOT:
-
-        # displaying the plot:
-        show()
-
-if SAVE_DATA:
-
-    # splitting the dataset into training and validation sets, after randomly
-    # shuffling samples and labels (keeping correspondences):
-    dataset = stack(
+    # taking a 2D projection of the Swiss Roll dataset, stacking
+    # samples and labels in the same array:
+    stacked_samples_and_labels = stack(
         (
-            dataset_samples[:, 0],
-            dataset_samples[:, 2],
-            dataset_labels
+            samples[:, 0],
+            samples[:, 2],
+            labels
         ),
         axis=-1
     )
-    dataset = shuffle(dataset)
-    split_sign = int(N_SAMPLES * (1 - VALIDATION_AMOUNT))
-    training_dataset = dataset[:split_sign,]
-    validation_dataset = dataset[split_sign:,]
+    return stacked_samples_and_labels
 
-    # saving each dataset to a CSV file where each row - but the first one,
-    # which is used only to count the number of columns - represents a sample
-    # whose respective features and its label are orderly reported in
-    # different columns:
-    training_dataset_path = os_join(
-        PROJECT_DIR,
-        "training_set.csv"
+
+def dress_plot() -> None:
+    """
+    Add axes' labels and colorbar to the current plot.
+    """
+    xlabel("Feature 1")
+    ylabel("Feature 2")
+    colorbar()
+
+
+def split_dataset(whole_dataset: ndarray) -> Tuple[ndarray, ndarray]:
+    """
+    Split the dataset into training and validation sets, after randomly
+    shuffling samples and labels (keeping correspondences).
+    """
+    seed(0)  # for a reproducible shuffling
+    shuffle(whole_dataset)
+    split_sign = int(N_SAMPLES * (1 - VALIDATION_AMOUNT))
+    training_set = whole_dataset[:split_sign,]
+    validation_set = whole_dataset[split_sign:,]
+    return training_set, validation_set
+
+if __name__ == '__main__':
+
+    # creating the dataset:
+    dataset = create_dataset()
+
+    # splitting the dataset into training and validation sets, after randomly
+    # shuffling samples and labels (keeping correspondences):
+    training_dataset, validation_dataset = split_dataset(
+        whole_dataset=dataset
     )
-    validation_dataset_path = os_join(
-        PROJECT_DIR,
-        "validation_set.csv"
-    )
-    # adding a fictitious initial sample with no data to each file just to
-    # count features:
-    training_dataset = insert(
-        arr=training_dataset,
-        obj=0,
-        values=[None, None, None],
-        axis=0
-    )
-    validation_dataset = insert(
-        arr=validation_dataset,
-        obj=0,
-        values=[None, None, None],
-        axis=0
-    )
-    savetxt(fname=training_dataset_path, X=training_dataset, delimiter=',')
-    savetxt(fname=validation_dataset_path, X=validation_dataset, delimiter=',')
+
+    if PLOT or SAVE_PLOTS:
+
+        # plotting the whole dataset samples in the 2D feature space,
+        # color-coded by label:
+        whole_dataset_figure = figure()
+        scatter(
+            x=dataset[:, 0],
+            y=dataset[:, 1],
+            c=dataset[:, 2]
+        )
+        dress_plot()
+
+        if SAVE_PLOTS:
+
+            # saving the plot picture:
+            picture_path = os_join(
+                PROJECT_DIR,
+                "readme_pictures",
+                "whole_dataset_plot.png"
+            )
+            title("Output Value as Color")
+            savefig(picture_path)
+
+        if PLOT:
+
+            # displaying the plot:
+            show()
+
+        # separately plotting the two separate sets' samples in the 2D feature
+        # space, color-coded by label:
+        separate_sets_figure = figure(figsize=(12, 4))
+        subplot(1, 2, 1)
+        scatter(
+            x=training_dataset[:, 0],
+            y=training_dataset[:, 1],
+            c=training_dataset[:, 2]
+        )
+        dress_plot()
+        title("Training Set")
+        subplot(1, 2, 2)
+        scatter(
+            x=validation_dataset[:, 0],
+            y=validation_dataset[:, 1],
+            c=validation_dataset[:, 2]
+        )
+        dress_plot()
+        title("Validation Set")
+
+        if SAVE_PLOTS:
+
+            # saving the plot picture:
+            picture_path = os_join(
+                PROJECT_DIR,
+                "readme_pictures",
+                "separate_sets_plot.png"
+            )
+            savefig(picture_path)
+
+        if PLOT:
+
+            # displaying the plot:
+            show()
+
+    if SAVE_DATA:
+
+        # saving each dataset to a CSV file where each row - but the first
+        # one, which is used only to count the number of columns - represents
+        # a sample whose respective features and its label are orderly
+        # reported in different columns:
+        training_dataset_path = os_join(
+            PROJECT_DIR,
+            "training_set.csv"
+        )
+        validation_dataset_path = os_join(
+            PROJECT_DIR,
+            "validation_set.csv"
+        )
+        # adding a fictitious initial sample with no data to each file just to
+        # count features:
+        training_dataset = insert(
+            arr=training_dataset,
+            obj=0,
+            values=[None, None, None],
+            axis=0
+        )
+        validation_dataset = insert(
+            arr=validation_dataset,
+            obj=0,
+            values=[None, None, None],
+            axis=0
+        )
+        savetxt(fname=training_dataset_path, X=training_dataset, delimiter=',')
+        savetxt(fname=validation_dataset_path, X=validation_dataset, delimiter=',')
